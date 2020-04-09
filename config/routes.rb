71f39849fa949ef9bff3bea77199cd8984d99bd3
Yuya <yuyatoyotomi@yahoo.co.jp>
2020-04-09 14:02:25 +0000

    Finish login-function

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-
   root 'static_pages#top'
   get '/signup', to: 'users#new'
+  
+  
+  #ログイン機能
+  get    '/login', to: 'sessions#new'
+  post   '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  
   resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
