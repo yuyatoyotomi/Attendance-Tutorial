ef8252ab74a959a3e71d673d368579bad8b2719f
Yuya <yuyatoyotomi@yahoo.co.jp>
2020-04-10 07:50:21 +0000

    Finish full-fledged-login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,7 +10,14 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
   
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+  
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
